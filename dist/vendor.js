5b1bbd536050274acf65531645e55ebf8f5d8261
Alberto Miranda <codealchemist@gmail.com>
2016-09-19 04:01:41 -0300

    new build

--- dist/vendor.js
+++ dist/vendor.js
@@ -7366,3 +7366,53 @@ else {
     }
   });
 })(this);
+
+function initSharing() {
+  var showSocialButtons = true;
+
+  // facebook
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '204380942916554',
+      xfbml      : true,
+      version    : 'v2.7'
+    });
+
+    FB.Event.subscribe('xfbml.render', onFacebookLoaded);
+
+    function onFacebookLoaded() {
+        if (!showSocialButtons) return;
+
+        console.log('-----------> FB LOADED');
+        jQuery('.social-button-container')
+            .removeClass('right-out')
+            .addClass('right-in');
+    }
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+  // twitter
+  window.twttr = (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],
+      t = window.twttr || {};
+    if (d.getElementById(id)) return t;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+
+    t._e = [];
+    t.ready = function(f) {
+      t._e.push(f);
+    };
+
+    return t;
+  }(document, "script", "twitter-wjs"));
+}
