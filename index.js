fd6f68074da7ad91acc7b582d5a839f6df2f185e
Alberto Miranda <codealchemist@gmail.com>
2016-09-20 15:26:54 -0300

    added google analytics; loading vendor assets properly.

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ var express = require('express');
 var http = require('http');
 var compression = require('compression');
 var app = express();
+var port = process.env.PORT || 5000;
 
 app.use(compression());
-app.use('/', express.static(__dirname + '/dist/app'));
-app.use('/gallery/*', express.static(__dirname + '/dist/app'));
-app.use('/our-picks', express.static(__dirname + '/dist/app'));
+app.use(express.static(__dirname + '/dist/app/'));
 app.use('/fonts', express.static(__dirname + '/dist/fonts'));
 app.use('/img', express.static(__dirname + '/dist/img'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(__dirname + '/dist/app/');
+});
 
 app.use(function(req, res){
   res.sendStatus(404);
 });
 
 var server = http.createServer(app);
-server.listen(process.env.PORT || 5000);
+server.listen(port);
+console.info(`
+--- server started on port ${port}
+`);
