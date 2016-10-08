f512ae3448d5f22558dd4ba95ac86af82a7d993d
Alberto Miranda <codealchemist@gmail.com>
2016-10-08 01:35:37 -0300

    added config

---
+++ src/components/config/config.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ConfigService {
+  private config = {}
+  private configMap = {
+    // development
+    'localhost': {
+      twitter: {
+        host: 'http://localhost:3080',
+        tweetsPerRequest: 10
+      }
+    },
+
+    // production
+    'www.atgallery.me': {
+      twitter: {
+        host: 'https://twitter-server.herokuapp.com',
+        tweetsPerRequest: 100
+      }
+    }
+  }
+
+  constructor () {
+    // set config aliases
+    this.configMap['atgallery.herokuapp.com'] = this.configMap['www.atgallery.me'];
+
+    let host = location.hostname;
+    if (!this.configMap[host]) throw new Error(`ERROR: Host not defined in ConfigService: ${host}.`);
+
+    this.config = this.configMap[host];
+  }
+
+  get (key) {
+    if (!this.config[key]) throw new Error(`ERROR: Config key not available: ${key}`);
+    return this.config[key];
+  }
+}
