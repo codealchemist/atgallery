e74d187976450e5b0ed20cc121b117dc904573f8
Alberto Miranda <codealchemist@gmail.com>
2016-10-15 15:12:26 -0300

    added twitter profile image proxy to support cards.

--- routes.js
+++ routes.js
@@ -1,12 +1,15 @@
 var requestify = require('requestify');
+var request = require('request').defaults({ encoding: null });
 module.exports = routes;
 
 var host = 'http://twitter-server.herokuapp.com';
+var profileImageBaseUrl = 'http://pbs.twimg.com/profile_images';
 
 // open graph defaults
 var openGraph = {
   username: '@atgalleryapp',
   image: 'http://www.atgallery.me/atg-logo.png',
+  proxiedImage: 'http://www.atgallery.me/atg-logo.png',
   title: 'Automatic Twitter Gallery',
   url: 'http://www.atgallery.me',
   description: 'Automatically generated image galleries for Twitter accounts.'
@@ -19,6 +22,25 @@ function routes(app) {
 
   app.get('/our-picks', (req, res) => {
     res.render('index', openGraph);
+  });
+
+  app.get('/twitter-profile-image/:id/:imageFile', (req, res) => {
+    // sample profile image urls
+    // http://pbs.twimg.com/profile_images/695361954018275330/RLTuGTD_.jpg
+    // http://pbs.twimg.com/profile_images/378800000411656087/f62c09abd8af33b66c72ac98dac96e63.jpeg
+
+    var id = req.params.id;
+    var imageFile = req.params.imageFile;
+    var url = `${profileImageBaseUrl}/${id}/${imageFile}`;
+    request(url, (error, response, body) => {
+      // error
+      if (error || response.statusCode !== 200) {
+        return res.end();
+      }
+
+      // OK!
+      return res.end(body, 'binary');
+    });
   });
 
   app.get('/search/:query', (req, res) => {
@@ -55,9 +77,12 @@ function routes(app) {
     })
     .then((response) => {
       var user = response.getBody();
+      var image = user.profile_image_url.replace('_normal', '');
+      var proxiedImage = image.replace(profileImageBaseUrl, `${openGraph.url}/twitter-profile-image`);
       var data = {
         username: `@${username}`,
-        image: user.profile_image_url.replace('_normal', ''),
+        image: image,
+        proxiedImage: proxiedImage,
         title: `${openGraph.title} for ${username}`,
         url: `${openGraph.url}/gallery/${username}`,
         description: user.description
